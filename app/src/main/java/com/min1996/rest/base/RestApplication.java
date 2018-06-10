5139edea6d6dfc1b1bee172a76fa7e5918e415a8
mrshareone <1243564554@163.com>
2018-06-10 18:16:08 +0800

    第一次提交，上帝保佑我这APP不要半路夭折。

---
+++ app/src/main/java/com/min1996/rest/base/RestApplication.java
@@ -0,0 +1,10 @@
+package com.min1996.rest.base;
+
+import android.app.Application;
+
+/**
+ * Autor：created by MR-SHAREONE on 2018/6/10 18 07
+ * Email:13437105740@163.com
+ */
+public class RestApplication extends Application {
+}
